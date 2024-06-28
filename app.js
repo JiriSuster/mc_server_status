fb531f860573ebb56e931ce79244361098eb20a2
Jiří Daniel Šuster <jiri.suster1@seznam.cz>
2024-06-28 14:21:58 +0200

    build test

--- app.js
+++ app.js
@@ -1,61 +1,154 @@
-const express = require("express");
+const express = require('express');
+const crypto = require('crypto');
+const mineflayer = require('mineflayer');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+// Function to generate a random 8-character username
+function generateRandomUsername() {
+    return crypto.randomBytes(4).toString('hex'); // Generates 8 hexadecimal characters
+}
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// Function to check for authentication
+async function checkForAuth(serverIP, serverPort, username, authType) {
+    return new Promise((resolve, reject) => {
+        let bot;
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+        try {
+            bot = mineflayer.createBot({
+                host: serverIP,
+                port: serverPort,
+                username: username,
+                auth: authType, // either 'offline' or 'mojang'
+            });
+        } catch (error) {
+            console.error(`Failed to create bot: ${error.message}`);
+            reject(new Error(`Failed to create bot: ${error.message}`));
+            return;
+        }
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
+        bot.on('kicked', (reason) => {
+            const reasonMessage = reason.toString(); // Convert reason to string for comparison
+            try {
+                const parsedReason = JSON.parse(reason);
+                console.log(parsedReason.translate);
+
+                if (parsedReason.translate === "multiplayer.disconnect.unverified_username") {
+                    resolve({ result: 'unverified_username' }); // Return "unverified_username"
+                } else if (reasonMessage.toLowerCase().includes("white")) {
+                    resolve({ result: 'whitelist' }); // Return "whitelist_issue"
+                } else {
+                    console.error(`Bot kicked: ${reasonMessage}`);
+                    reject(new Error(`Bot kicked: ${reasonMessage}`));
+                }
+            } catch (e) {
+                console.error(`Failed to parse kick reason: ${reasonMessage}`);
+                reject(new Error(`Failed to parse kick reason: ${reasonMessage}`));
+            }
+            if (bot && typeof bot.quit === 'function') {
+                bot.quit();
+            }
         });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+
+        bot.on('error', err => {
+            console.error(`Bot encountered an error: ${err.message}`);
+            reject(new Error(`Bot encountered an error: ${err.message}`));
+            if (bot && typeof bot.quit === 'function') {
+                bot.quit();
+            }
+        });
+
+        bot.on('login', () => {
+            console.log(`Bot logged in as ${username}`);
+            bot.chat("asdfasdf"); // Attempt to send a message to the chat
+
+            let messageSent = false;
+
+            bot.on('message', (jsonMsg, position) => {
+                const message = jsonMsg.toString();
+                console.log(`Received message: ${message}`);
+                if (message.toLowerCase().includes("asdfasdf")) {
+                    messageSent = true;
+                    resolve({ result: 'success' }); // If message is found, return "success"
+                    if (bot && typeof bot.quit === 'function') {
+                        bot.quit();
+                    }
+                } else if (message.toLowerCase().includes('regis') || message.toLowerCase().includes('auth')) {
+                    messageSent = true;
+                    resolve({ result: 'auth' }); // If message contains "regis" or "auth", return "auth"
+                    if (bot && typeof bot.quit === 'function') {
+                        bot.quit();
+                    }
+                }
+            });
+
+            // Timeout after a reasonable period to avoid hanging indefinitely
+            setTimeout(() => {
+                if (!messageSent) {
+                    reject(new Error('Message was not sent successfully or not received within timeout period'));
+                    if (bot && typeof bot.quit === 'function') {
+                        bot.quit();
+                    }
+                }
+            }, 10000); // 10 seconds timeout
+        });
+    });
+}
+
+// Route to check if server has login plugin
+const router = express.Router();
+router.get('/checkServerStatus', async (req, res) => {
+    const serverIP = req.query.ip ? req.query.ip.split(':')[0] : '';
+    const serverPort = req.query.ip ? parseInt(req.query.ip.split(':')[1], 10) : 25565;
+    const username = generateRandomUsername();
+    const authType = req.query.authType || 'offline'; // Default to offline mode
+
+    try {
+        const { result } = await checkForAuth(serverIP, serverPort, username, authType);
+
+        // Return result based on checkForAuth response
+        res.send(result);
+    } catch (error) {
+        console.error('Error occurred:', error.message);
+        // Return the error message for unspecified errors
+        res.status(500).send(error.message);
+    }
+});
+
+// Route to check if the server is running
+router.get('/isServerRunning', async (req, res) => {
+    const serverIP = req.query.ip ? req.query.ip.split(':')[0] : '';
+    const serverPort = req.query.ip ? parseInt(req.query.ip.split(':')[1], 10) : 25565;
+
+    try {
+        const bot = mineflayer.createBot({
+            host: serverIP,
+            port: serverPort,
+            username: 'checkBot',
+            auth: 'offline',
+        });
+
+        bot.on('login', () => {
+            bot.quit();
+            res.send('Server is running');
+        });
+
+        bot.on('error', err => {
+            res.status(500).send(`Error: ${err.message}`);
+        });
+
+        setTimeout(() => {
+            bot.quit();
+            res.status(500).send('Server did not respond in time');
+        }, 10000); // 10 seconds timeout
+    } catch (error) {
+        res.status(500).send(`Failed to create bot: ${error.message}`);
+    }
+});
+
+app.use("/api/", router);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
